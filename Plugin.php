09293926e2b056af8b7292b1c1c553640ce4fcfe
patrickkusebauch <patrick.kusebauch@gmail.com>
2020-10-25 06:12:50 +0100

    Added more stub files from PHPStan Nette plugin

--- Plugin.php
+++ Plugin.php
@@ -14,10 +14,20 @@ class Plugin implements PluginEntryPointInterface
             $psalm->addStubFile($file);
         }
     }
-
     /** @return array<string> */
     private function getStubFiles(): array
     {
-        return glob(__DIR__ . '/stubs/*.phpstub') ?: [];
+        $phpstan = self::rglob(__DIR__ . '/../../phpstan/phpstan-nette/stubs/*/*.stub') ?: [];
+        $mine = glob(__DIR__.'/stubs/*.phpstub') ?: [];
+        return $phpstan + $mine;
     }
+
+    private static function rglob($pattern, $flags = 0) {
+        $files = glob($pattern, $flags);
+        foreach (glob(dirname($pattern).'/*', GLOB_ONLYDIR|GLOB_NOSORT) as $dir) {
+            $files = array_merge($files, self::rglob($dir.'/'.basename($pattern), $flags));
+        }
+        return $files;
+    }
+
 }
